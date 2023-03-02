96f3cdf4b324b56f3948af4131cbb0a3eae9a2cb
unknown <mwangiarthur777@gmail.com>
2023-03-02 16:25:06 +0300

    second commit

--- src/components/homehero/HomeHero.jsx
+++ src/components/homehero/HomeHero.jsx
@@ -5,18 +5,16 @@ function HomeHero() {
   return (
     <div className='homehero flex items-center justify-center'>
         <div className='container'>
-            <div className='homehero-left'>
+            <div className='homehero-wrapper'>
                 <p className='text-6xl text-amber-500 font-black mb-3'>SkillsHub.</p>
                 <p className='text-5xl font-bold text-white'>Simplify the process of finding skilled professionals for any job in your location. </p>
-                <div className='flex flex-col'>
-                    <input className='skill-s-input mb-2 px-2 py-3' type='text'/>
-                    <input className='skill-s-input mb-2 px-2 py-3' type='Type Location'/>
-                    <button className='mt-4'>Search</button>
+                <div className='flex flex-col mt-4'>
+                    <input className='skill-s-input mb-2 px-2 py-3 outline-0' type='text' placeholder='Search a skill'/>
+                    <input className='skill-s-input mb-25 px-2 py-3 outline-0' type='Type Location' placeholder='Location'/>
+                    <button className='flex  bg-amber-500 self-start search-btn bg-red-500 mt-2 py-3 px-10 rounded-full text-center'>Search</button>
                 </div>
             </div>
-            <div className='homehero-right'>
-
-            </div>
+           
         </div>
     </div>
   )
