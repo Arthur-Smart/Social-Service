291b3af5b8bc5d1a29f00203c77dfe20160236f4
unknown <mwangiarthur777@gmail.com>
2023-03-10 18:58:25 +0300

    new commit

--- src/pages/createjob/CreateJob.jsx
+++ src/pages/createjob/CreateJob.jsx
@@ -4,6 +4,10 @@ import './createjob.css'
 function CreateJob() {
   return (
     <div className='create-job flex flex-col items-center justify-center'>
+        <div className='container mt-10 mb-3 flex items-center'>
+            <p className='font-medium text-xl text-zinc-500'>Post a new skill engagement</p>
+            <p className='font-medium text-xl text-zinc-500 ml-4 text-indigo-700 cursor-pointer underline'><i class="fa-solid fa-chevron-left"></i> Post job instead</p>
+        </div>
         <div className='container'>
 
         </div>
