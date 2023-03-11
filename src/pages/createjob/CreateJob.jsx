c3c4d8f7ccfc5a4168e175f15f6cc9838b548e1c
unknown <mwangiarthur777@gmail.com>
2023-03-11 13:07:27 +0300

    new commit

--- src/pages/createjob/CreateJob.jsx
+++ src/pages/createjob/CreateJob.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './createjob.css'
 
 function CreateJob() {
-  return (
+      return (
     <div className='create-job flex flex-col items-center justify-center '>
         <div className='container mt-10 mb-3 flex items-center'>
             <p className='font-medium text-xl text-zinc-500 text-indigo-700 cursor-pointer underline'>Post a new skill engagement <i class="fa-solid fa-chevron-right"></i></p>
@@ -23,9 +23,20 @@ function CreateJob() {
             <div className='c-job-description px-3 pb-2'>
                 <textarea className='py-2 px-2  outline-1 outline-amber-100 text-zinc-500' placeholder='Job description'></textarea>
             </div>
-            <div className='add-skil'>
+            <div className='add-skill px-3'>
                 <p>Specify mandatory skill requirements</p>
-                <input type='text' placeholder='Type skill requirement'/>
+                <div className='title-add flex items-center justify-between'>
+                    <input
+                        className='py-2 px-2  outline-1 outline-amber-100 text-zinc-500'
+                        type='text'
+                        placeholder='Type skill requirement'/>
+                    <button  className='py-2 px-4 bg-amber-500 outline-amber-100'>Add</button>
+                </div>
+                <div className='skills mt-4 flex gap-2'>
+                    <p className='skill-text rounded-full border-2 border-amber-500 py-3 px-5'>Python</p>
+                    <p className='skill-text rounded-full border-2 border-amber-500 py-3 px-5'>JavaScript</p>
+                    <p className='skill-text rounded-full border-2 border-amber-500 py-3 px-5'>SQL</p>
+                </div>
             </div>
         </div>
     </div>
