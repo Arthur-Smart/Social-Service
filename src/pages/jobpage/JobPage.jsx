b928423abb9c345d4dbee118b31f1655d3c15b52
unknown <mwangiarthur777@gmail.com>
2023-03-11 19:30:54 +0300

    new commit

--- src/pages/jobpage/JobPage.jsx
+++ src/pages/jobpage/JobPage.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import OtherJobs from '../../components/otherjobs/OtherJobs'
 import './jobpage.css'
 
 function JobPage() {
+    const [open, setOpen] = useState(false)
   return (
     <div className='jobpage flex flex-col items-center justify-center'>
         <div className='container flex items-center justify-center mt-10'>
@@ -36,14 +37,14 @@ function JobPage() {
                     <p className='font-medium text-zinc-500'>Bugeted payment</p>
                     <p className='text-zinc-500'><i class="fa-solid fa-coins"></i> Kes 20,000</p>
                 </div>
-                <button className='ml-3 apply-btn py-3 px-4 rounded-full text-white'>Apply</button>
+                <button onClick={() => setOpen(true)} className='ml-3 apply-btn py-3 px-4 rounded-full text-white'>Apply</button>
             </div>
         </div>
 
         {/*Same clients job*/}
         <div className='similar-jobs mt-10 flex items-center justify-center py-2'>
             <div className='container'>
-                <p className='text-amber-500  text-xl similar-title mt-4'>Other job posting by John Mwaniki | <span className='text-white font-bold'>20 other jobs</span></p>
+                <p className='text-white  text-lg similar-title mt-4'>Other job posting by John Mwaniki | <span className='text-amber-500 font-bold'>20 other jobs</span></p>
                 <div className='other-jobs-wrapper flex flex-wrap items-center gap-2 justify-center'>
                     <OtherJobs/>
                     <OtherJobs/>
@@ -53,21 +54,24 @@ function JobPage() {
         </div>
 
         {/*Apply module */}
-        <div className='apply flex items-center'>
-            <div className='apply-wrapper bg-white p-3 flex flex-col items-center justify-center'>
+        {open && (
+            <div className='apply flex flex-col items-center justify-center'>
+            <div className='apply-wrapper rounded-md bg-white p-3 flex flex-col items-center justify-center'>
                 <input className='py-2 px-2 border-2 border-indigo-200 outline-0 text-zinc-500 w-full' type='text' placeholder='Enter email'/>
                 <input className='py-2 px-2 border-2 border-indigo-200  outline-0 text-zinc-500 w-full mt-2' type='text' placeholder='Enter phone number'/>
                 <textarea className='apply-textarea py-2 px-2 border-2 border-indigo-200  outline-0 text-zinc-500 mt-2' placeholder='Write a proposal'></textarea>
                 <input type='file' id='upload' style={{display:'none'}}/>
                 <div className='w-full items-start flex items-center py-2 justify-between'>
                    <label className='flex-start border-2 text-white cursor-pointer border-amber-100 rounded-full py-2 px-5 mt-2 bg-amber-500' htmlFor='upload'>Upload Resume</label>
-                    <button className='item-start  apply-btn py-2 px-4 rounded-full text-white'>Publish</button>
+                    <button disabled className='item-start  apply-btn py-2 px-4 rounded-full text-white'>Publish</button>
                 </div>                
             </div>
-            <p>close</p>
-            <div>                
+            <div className='items-start close-apply-btn'>    
+                 <p onClick={() => setOpen(false)} className='text-white text-lg cursor-pointer'><i class="fa-solid fa-rectangle-xmark"></i></p>            
             </div>
         </div>
+        ) }
+        
     </div>
   )
 }
