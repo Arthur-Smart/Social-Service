3f3c651791629cd406b1eceb7bedaef1702d5d25
unknown <mwangiarthur777@gmail.com>
2023-03-23 19:09:11 +0300

    new commit

--- src/components/carousel/ServicesCarousel.jsx
+++ src/components/carousel/ServicesCarousel.jsx
@@ -4,11 +4,22 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import AliceCarousel from 'react-alice-carousel';
 import './servicecarousel.css'
 import ServiceCard from '../servicecard/ServiceCard';
+import { useQuery } from '@tanstack/react-query'
+import axios from "axios"
 
 function ServicesCarousel() {
+  const { isLoading, error, data, refetch } = useQuery({
+        queryKey: ['randomservice'],
+        queryFn: () =>
+        axios(`http://localhost:8800/api/service/`).then((res) => {
+            return res.data;
+      })
+  });
 
- const data = servicesData;
- const items = data.map((item) => (
+  const randomData = data?.sort(() => Math.random() - 0.5).slice(0, 10);
+
+ 
+ const items = randomData?.map((item) => (
   <ServiceCard key={item.id} item={item} />  
  ))
 
@@ -25,9 +36,8 @@ function ServicesCarousel() {
         <div className='container'>
             <p className='text-3xl font-bold text-zinc-600'>Find Your Next Skilled Partner for Success</p>
             <p className='text-zinc-500'>Discover the Satisfying and Fulfilling Experience of Finding Amazing Skills in Kenya and Your Location</p>
-        </div>        
-        <div className='container flex items-center flex-col mt-5'>
-       
+        </div>{isLoading ? 'Loading' : error ? 'something went wrong' : (
+          <div className='container flex items-center flex-col mt-5'>
              <AliceCarousel 
                 mouseTracking
                 infinite
@@ -39,6 +49,8 @@ function ServicesCarousel() {
                 items={items}
               />
         </div>
+        )}        
+        
         <div className='container flex items-start'>
           <button className='border-2 border-indigo-800 hover:bg-indigo-800 hover:text-white py-3 px-10 rounded-full '>More</button>
         </div>
--- src/components/servicecard/ServiceCard.jsx
+++ src/components/servicecard/ServiceCard.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import './servicecard.css'
 import {Link} from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import axios from "axios"
 
 function ServiceCard({item}) {
+  const { isLoading, error, data, refetch } = useQuery({
+        queryKey: ['userJobOwner'],
+        queryFn: () =>
+        axios(`http://localhost:8800/api/user/${item.userId}`).then((res) => {
+            return res.data;
+      })
+  });
   return (
-    <Link to='/engagement/4'>
+    <Link to={`/engagement/${item._id}`}>
       <div className='service-card border-gray-200 s-card-border cursor-pointer'>
-          <img className='service-img' src={item.img} alt='skills hub'/>
+          <img className='service-img' src={item?.image} alt='skills hub'/>
           <div className='name-profile flex items-center p-2'>
-              <img className='service-owner-pic' src={item.profilePic} alt='skills hub'/>
-              <p className='ml-2 text-zinc-500'>{item.author}</p>
+              <img className='service-owner-pic' src={data?.userImage} alt='skills hub'/>
+              <p className='ml-2 text-zinc-500'>{data?.name}</p>
           </div>
           <div className='name-profile px-2'>
-              <p className='ml-2 text-zinc-800 text-xl font-bold'>{item.title}</p>
-              <p className='ml-2 text-zinc-500'>I design cool graphic solutions from logos, brochure, magazines...</p>
+              <p className='ml-2 text-zinc-800 text-xl font-bold'>{item?.title}</p>
+              <p className='ml-2 text-zinc-500'>{item?.description.substring(0,80)}...</p>
           </div>
           <div className='price-tab bg-amber-500'>
-              <p className='text-white'>Kes 2000</p>
+              <p className='text-white'>Kes {item?.price}</p>
           </div>
       </div>
     </Link>
