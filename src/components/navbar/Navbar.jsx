0370dc83d5c58a0f878dafc01c8a3ab25f236b0a
unknown <mwangiarthur777@gmail.com>
2023-03-26 19:14:21 +0300

    new commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,10 +6,14 @@ import axios from 'axios'
 import './navbar.css'
 
 function Navbar() {
+    const [open, setOpen] = useState(false)
     const [openRegModel, setOpenRegModel] = useState(false)
     const [openLoginModel, setOpenLoginModel] = useState(false)
     const user = JSON.parse(localStorage.getItem("currentUser"));
-    const navigate = useNavigation()
+
+    const openMobileMenu = () => {
+        setOpen(!open)
+    }
 
     const logoutUser = () => {
         try {
@@ -23,17 +27,17 @@ function Navbar() {
     
   return (
     <div className='navbar flex items-center justify-center'>
-        <div className='container flex items-center justify-between'>
+        <div className='container main-n-wrapper flex items-center justify-between'>
             <div className='left-nav'>
                 <Link to='/'><p className='font-bold text-3xl cursor-pointer'>Skills<span className='text-amber-500'>Hub</span></p></Link>
             </div>
             <div className='right-nav flex items-center justify-end'>
                <Link to='/engagements' ><p className='mr-6 text-lg font-medium text-amber-500 cursor-pointer ' >Find a skill</p></Link>
-                <p className='mr-6 text-lg font-medium text-amber-500 cursor-pointer ' >Why SkillsHub</p>
+                <p className='mr-6 text-lg font-medium text-amber-500 cursor-pointer ' >Get a paying task</p>
                 {user  !==null ? (
                     <>
                         <button onClick={() => logoutUser()} className='mr-6 border-amber-500 rounded-full py-2 px-8 border-2 text-lg font-medium'>Logout</button>
-                        <button onClick={() => setOpenRegModel(true)} className='bg-black px-8 py-2 border-black border-2 rounded-full text-lg font-medium text-amber-500'>Create post</button>
+                        <button onClick={() => setOpenRegModel(true)} className='bg-black px-8 py-2 border-black border-2 rounded-full text-lg font-medium text-amber-500'><i class="fa-solid fa-plus"></i></button>
                     </>
                 ) : 
                     (
@@ -48,15 +52,15 @@ function Navbar() {
                 <div className='flex items-center notifications bg-amber-500 ml-6'>
                    {user && <img className='user-nav-pic' src={user?.userImage} alt='Skills hub'/>}
                 </div>
-                </Link>
+                </Link>                
             </div>
-            
+            <p onClick={() => openMobileMenu()} className='bars flex text-3xl text-zinc-600 cursor-pointer'><i class="fa-solid fa-bars-staggered fa-rotate-180"></i></p>
         </div>
         {openRegModel && <Register setOpenRegModel={setOpenRegModel}/>}
         {openLoginModel && <Login setOpenLoginModel={setOpenLoginModel}/>}
-        <div className='mobile-view '>
-            <div className='mobile-i-wrapper px-3 py-3'>
-                 <p className='mb-8 cursor-pointer font-bold text-xl text-indigo-500'><i class="fa-solid fa-arrow-right-from-bracket fa-rotate-180"></i></p>
+        <div className={open ? 'mobile-view show' : 'mobile-view'}>
+            <div className='mobile-i-wrapper px-3 py-3'>                
+                 <p onClick={() => openMobileMenu()} className='mb-8 cursor-pointer font-bold text-xl text-indigo-500'><i class="fa-solid fa-arrow-right-from-bracket fa-rotate-180"></i></p>
                  <div className='flex items-center'>
                     <img className='mobile-a-user' src={require('../../assets/mainbg.jpg')} alt="Skills hub"/>
                     <div className='ml-1'>
